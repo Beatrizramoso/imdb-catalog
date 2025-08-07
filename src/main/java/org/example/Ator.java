a821cb056b7f49cece800e7d60162c8c311971af
Beatriz <beatrizramoso2024@gmail.com>
2025-08-06 21:19:06 -0300

    Ajuste classe Ator

---
+++ src/main/java/org/example/Ator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Ator
+{
+    public String nome;
+    public String instagram;
+    public String personagemMaisPopular;
+
+    public Ator(String nome, String instagram, String personagemMaisPopular) {
+        this.nome = nome;
+        this.instagram = instagram;
+        this.personagemMaisPopular = personagemMaisPopular;
+    }
+
+}
