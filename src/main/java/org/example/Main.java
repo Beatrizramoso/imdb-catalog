0cdd40892fa92e7c41779ddff90180f652689572
Beatriz <beatrizramoso2024@gmail.com>
2025-08-06 21:33:35 -0300

    Implementa cadastro de atores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Application initiated!!!");
 
+        // Cadastro de atores
+        Ator leonardoDiCaprio = new Ator(
+                "Leonardo DiCaprio",
+                "@leonardodicaprio",
+                "Calvin Candie");
+        Ator bradPitt = new Ator(
+                "Brad Pitt",
+                "@bradpittofflcial",
+                "Tenente Aldo Raine");
+
         System.out.println("Application finished!!!!");
     }
 
