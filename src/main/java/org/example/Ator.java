95e03d8ebacdffd155748759ddb131f9fede7b9b
Andre <breitfelder@hotmail.com>
2025-08-08 16:38:14 -0300

    Inserção de comentário na Classe

--- src/main/java/org/example/Ator.java
+++ src/main/java/org/example/Ator.java
@@ -1,15 +1,16 @@
 package org.example;
 
+//Criação da Classe Ator
 public class Ator {
 
     //Atributo nome;
     public String nome;
-
     //Atributo Instagram
     public String instagram;
-
+    //Atributo Personagem Popular
     public String personagemMaisPopular;
 
+    //*Construtor do Método cadastra Ator*//
     public Ator(String nome, String instagram, String personagemMaisPopular) {
         this.nome = nome;
         this.instagram = instagram;
